697b81f3f5ab0385b18acd51292c3c615a205988
craig-mini <cm.nicol@outlook.com>
2020-09-18 17:14:36 +0100

    set up admin site and super user

--- capital_call/app/admin.py
+++ capital_call/app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DataFund, DataCommitment, DataCall, DataFundInvestment
+admin.site.register([DataFund, DataCommitment, DataCall, DataFundInvestment])
