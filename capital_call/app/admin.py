3481258304ee380d4b603dc30299d8aaa5285cff
craig-mini <cm.nicol@outlook.com>
2020-09-18 17:15:14 +0100

    Untrack files in .gitignore

--- capital_call/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import DataFund, DataCommitment, DataCall, DataFundInvestment
-admin.site.register([DataFund, DataCommitment, DataCall, DataFundInvestment])
