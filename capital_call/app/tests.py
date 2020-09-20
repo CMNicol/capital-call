8aad700868c1155dbc970daaf5fa313c71b7e846
craig-mini <cm.nicol@outlook.com>
2020-09-20 15:15:08 +0100

    remove db setup in tests

--- capital_call/app/tests.py
+++ capital_call/app/tests.py
@@ -33,19 +33,6 @@ class TestModels(TestCase):
         investment_amount=9500000
     )
 
-    def setUp(cls):
-        DataFund(fundName='Fund 1').save()
-        DataFund(fundName='Fund 2').save()
-        DataFund(fundName='Fund 3').save()
-        DataFund(fundName='Fund 4').save()
-        DataFund(fundName='Fund 5').save()
-
-        DataCommitment(fund_id=DataFund.objects.get(fundName='Fund 1'), date=date(2017, 12, 31), amount=10000000).save()
-        DataCommitment(fund_id=DataFund.objects.get(fundName='Fund 2'), date=date(2018, 3, 31), amount=15000000).save()
-        DataCommitment(fund_id=DataFund.objects.get(fundName='Fund 3'), date=date(2018, 6, 30), amount=10000000).save()
-        DataCommitment(fund_id=DataFund.objects.get(fundName='Fund 4'), date=date(2018, 9, 30), amount=15000000).save()
-        DataCommitment(fund_id=DataFund.objects.get(fundName='Fund 1'), date=date(2018, 12, 31), amount=10000000).save()
-
     def test_foreign_key_funds_and_commitments(self):
         fundFromDataFund = DataFund.objects.get(fundName='Fund 2')
         fundFromDataCommitment = DataCommitment.objects.get(fund_id=fundFromDataFund)
