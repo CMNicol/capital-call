8f5e76b980ab8188c48ab40b50e705a234f178a6
craig-mini <cm.nicol@outlook.com>
2020-09-20 14:46:07 +0100

    refactor and comment some

--- capital_call/app/callcontroller.py
+++ capital_call/app/callcontroller.py
@@ -6,12 +6,15 @@ from datetime import date
 class CallController:
 
     def __init__(self, date: date, investment_name: str, capital_required: int):
-        self.date = date
+        self.date = date  # date of call
         self.investment_name = investment_name
-        self.capital_required = capital_required
-        self.new_dfi_entries = []
-        self.preview_table_data = []
-        self.call = DataCall
+        self.capital_required = capital_required  # capital required by the call
+        self.new_dfi_entries = []  # DataFundInvestment entries based on the call
+        self.preview_table_data = []  # list used to populate the call preview table
+
+    # add_all()
+    # This method calculates the sum of the 'investment_amount' fields of all records
+    # in records.
 
     def add_all(self, records: QuerySet) -> int:
         total = 0
@@ -19,8 +22,12 @@ class CallController:
             total += record.investment_amount
         return total
 
-    def calculate_call(self, is_confirmed: bool):
+    # calculate_call()
+    # This method calculates the capital to be drawn from which commitments and funds.
+    # It is configurable to either add new records to the database or calculate a proposed call
+    # through the is_confirmed argument.
 
+    def calculate_call(self, is_confirmed: bool):
         capital_to_find = self.capital_required
         commitments = DataCommitment.objects.all()
         # create new DataCall record
@@ -41,6 +48,7 @@ class CallController:
             if undrawn_capital == 0:
                 pass
             elif capital_to_find < undrawn_capital:
+                # the undrawn capital commitment in this commitment will fulfil the remaining capital required
                 if is_confirmed:
                     self.new_dfi_entries.append(
                         DataFundInvestment(
@@ -62,6 +70,7 @@ class CallController:
                     )
                 break
             else:
+                # the undrawn capital commitment in this commitment will be drawn entirely
                 capital_to_find = capital_to_find - undrawn_capital
                 if is_confirmed:
                     self.new_dfi_entries.append(
@@ -85,6 +94,10 @@ class CallController:
         if is_confirmed:
             for entry in self.new_dfi_entries:
                 entry.save()
+
+    # preview_table_data_entry()
+    # This method returns a dictionary of its arguments.
+    # The dictionary represents a single row of the call preview table
 
     def preview_table_data_entry(self, commitment: DataCommitment, undrawn_before_drawdown: int, drawdown_notice: int,
                                  undrawn_after_drawdown: int) -> dict:
