a8990a3d3729673d4d61111b33a3685a979bd2c1
craig-mini <cm.nicol@outlook.com>
2020-10-02 12:10:30 +0100

    make get_dashboard_data part of Dashboard class

--- capital_call/app/views.py
+++ capital_call/app/views.py
@@ -14,24 +14,6 @@ def error(request):
     return render(request, 'error.html')
 
 
-def get_dashboard_data():
-    calls = DataCall.objects.all()
-    call_fund_amount_list = []
-    column_headers = ['Date', 'Call ID']
-    column_headers += list(DataFund.objects.values_list('fundName', flat=True))
-    for call in calls:
-        call_fund_amount = [call.date, call.pk]
-        for fund in DataFund.objects.all():
-            dfi = DataFundInvestment.objects.filter(call_id=call).filter(fund_id=fund).first()
-            if dfi:
-                call_fund_amount.append(dfi.investment_amount)
-            else:
-                call_fund_amount.append(0)
-
-        call_fund_amount_list.append(call_fund_amount)
-    return call_fund_amount_list, column_headers
-
-
 class Create(APIView):
     def post(self, request: HttpRequest):
         form = CallForm(request.POST)
@@ -47,18 +29,32 @@ class Create(APIView):
             return render(request, 'call_confirm_invalid.html')
 
     def get(self, request: HttpRequest):
-        return self.__render_empty_form(request=request)
-
-    def __render_empty_form(self, request: HttpRequest) -> HttpResponse:
         form = CallForm()
         return render(request, 'create_call.html', {'form': form})
 
 
 class Dashboard(APIView):
     def get(self, request: HttpRequest):
-        call_fund_amount_list, column_headers = get_dashboard_data()
+        call_fund_amount_list, column_headers = self.__get_dashboard_data()
         return render(request, 'dashboard.html',
                       {'dashboard_data': call_fund_amount_list, 'column_headers': column_headers})
+
+    def __get_dashboard_data(self):
+        calls = DataCall.objects.all()
+        call_fund_amount_list = []
+        column_headers = ['Date', 'Call ID']
+        column_headers += list(DataFund.objects.values_list('fundName', flat=True))
+        for call in calls:
+            call_fund_amount = [call.date, call.pk]
+            for fund in DataFund.objects.all():
+                dfi = DataFundInvestment.objects.filter(call_id=call).filter(fund_id=fund).first()
+                if dfi:
+                    call_fund_amount.append(dfi.investment_amount)
+                else:
+                    call_fund_amount.append(0)
+
+            call_fund_amount_list.append(call_fund_amount)
+        return call_fund_amount_list, column_headers
 
 
 class Confirm(APIView):
@@ -74,4 +70,3 @@ class Confirm(APIView):
             return render(request, 'call_confirmed.html')
         else:
             return render(request, 'call_confirm_invalid.html')
-
